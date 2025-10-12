3d1c5051a91bb69d29004723dc5cd36bf47d2463
Eugene Afriyie <groupeight00@gmail.com>
2025-10-12 23:28:15 +0000

    installinf dpendency

---
+++ forex_academy_site/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#001F3F',
+        neonGreen: '#39FF14',
+        gold: '#FFD700',
+        gray: '#A9A9A9',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
