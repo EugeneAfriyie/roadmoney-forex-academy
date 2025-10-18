cdf74031b0682a9287c55fd28a1c29a7fd1c64b1
Eugene Afriyie <groupeight00@gmail.com>
2025-10-18 01:45:27 +0000

    firebase hosting

--- forex_academy_site/vite.config.ts
+++ forex_academy_site/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // ⬇ Disable TypeScript type checking during build
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+});
